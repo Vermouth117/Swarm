86b10f93637c9912114b01368cd492a97ca5f864
Kohei-Mori2018 <kohei998morizoo@gmail.com>
2024-07-06 21:58:36 +0900

    voice iframe link作成

--- src/components/Voice.tsx
+++ src/components/Voice.tsx
@@ -3,7 +3,13 @@ import Footer from "./Footer.tsx";
 export default function Voice() {
   return (
     <section>
-      <h1>Voice</h1>
+        <iframe
+            src="https://d3b8mr1wm8ww1d.cloudfront.net/"
+            width="100%"
+            height="600px"
+            title="External Page"
+        >
+        </iframe>
       <Footer />
     </section>
   );
